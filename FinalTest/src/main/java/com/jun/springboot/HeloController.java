909366bc8a2e5a87cb9eef75dee1f34f792f5097
YangHyeonJun <tiskqmfls@naver.com>
2019-06-17 22:38:24 +0900

    Controller 추가

---
+++ FinalTest/src/main/java/com/jun/springboot/HeloController.java
@@ -0,0 +1,18 @@
+package com.jun.springboot;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.jun.springboot.repositories.MyDataMongoRepository;
+
+@Controller
+public class HeloController {
+
+}
